17725ec283c8c74be64e4eac1d7208362d013d6e
ShivakumarDornala <shivakumardornla000@gmail.com>
2025-07-01 23:32:36 +0530

    Make some changes

--- src/Components/AppTodo.jsx
+++ src/Components/AppTodo.jsx
@@ -1,54 +1,53 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { IoMdAdd } from "react-icons/io";
 
 function AppTOdo({ onNewChange }) {
-  const [todoName, settodoName] = useState();
-  const [dueDate, setDueDate] = useState();
+  const todoNameelement = useRef();
+  const dueDateelement = useRef();
 
-  const handleNameChange = (event) => {
-    settodoName(event.target.value);
-  };
-
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
-
-  const handleAddButtonClicked = () => {
-    if (todoName === "" || dueDate === "") {
+  const handleAddButtonClicked = (event) => {
+    if (
+      todoNameelement.current.value === "" ||
+      dueDateelement.current.value === ""
+    ) {
       alert("You must enter a TODO and a due date");
-      return; // Prevent further execution
+      return;
+    } else {
+      event.preventDefault();
+      const todoName = todoNameelement.current.value;
+      const dueDate = dueDateelement.current.value;
+      todoNameelement.current.value = "";
+      dueDateelement.current.value = "";
+      onNewChange(todoName, dueDate);
     }
-
-    onNewChange(todoName, dueDate);
-    settodoName("");
-    setDueDate("");
   };
 
   return (
     <>
-      <div className="flex flex-col md:flex md:flex-row md:items-center md:justify-center mt-5 gap-6">
+      <form
+        onSubmit={handleAddButtonClicked}
+        className="flex flex-col md:flex md:flex-row md:items-center md:justify-center mt-5 gap-6"
+      >
         <input
           type="text"
+          ref={todoNameelement}
           placeholder="Enter your TODO"
           className="py-2 px-3 border-2 border-gray-300 rounded"
           required
-          value={todoName}
-          onChange={handleNameChange}
         />
         <input
           type="date"
+          ref={dueDateelement}
           className="py-2 px-3 border-2 border-gray-300 rounded cursor-pointer"
           required
-          value={dueDate}
-          onChange={handleDateChange}
         />
         <button
+          type="submit"
           className="py-2 px-3 text-white bg-blue-500 rounded  text-[20px] h-[40px] cursor-pointer flex items-center justify-center"
-          onClick={handleAddButtonClicked}
         >
           <IoMdAdd></IoMdAdd>
         </button>
-      </div>
+      </form>
     </>
   );
 }
